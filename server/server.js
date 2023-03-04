9252c40465726b1064832b28f2e7e2e18fa49b28
Daniel Martin <51744227+DanielMartin971@users.noreply.github.com>
2023-03-04 01:43:17 -0800

    changed the server.js to be used with Apollo Server

--- server/server.js
+++ server/server.js
@@ -1,10 +1,20 @@
 const express = require('express');
-const path = require('path');
-const db = require('./config/connection');
-const routes = require('./routes');
+const db      = require('./config/connection');
+const path    = require('path');
+const { ApolloServer }        = require('apollo-server-express');
+const { authMiddleWare }      = require('./utils/auth');
+const { typeDefs, resolvers } = require('./schemas');
 
-const app = express();
+const app  = express();
 const PORT = process.env.PORT || 3001;
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleWare
+});
+
+server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -14,8 +24,11 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-// app.use(routes);
-
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`🌍 Now listening on localhost:${PORT}`)
+
+    // Log where we can test our GQL API
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
 });
